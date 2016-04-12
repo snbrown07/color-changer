69e73863ccb7c988e816bc7d33f1112f68d8563a
megknoll <megknoll@gmail.com>
2016-04-11 23:05:24 -0400

    Create tests.js

---
+++ js/tests.js
@@ -0,0 +1,45 @@
+QUnit.module("Color Change", function() {
+  //COLORS ARRAY
+    test("colors array", function() {
+    ok(typeof colors !== "undefined", "Colors variable is properly defined.");
+    ok(Object.prototype.toString.call(colors) === '[object Array]',"Colors is a valid array");
+    ok(colors.length >= 4, "Colors array has at least 4 items");
+    
+    var check = 0;
+    for(var i = 0;i<colors.length;i++){
+      if(typeof colors[i] != 'string'){
+        check++;
+      }
+    }
+    
+    ok(check === 0 && colors.length > 0, "All items in the colors array are strings");
+    
+    expect(4);
+  });
+  
+    //CHANGE COLOR
+  test("changeColor()", function(){
+  ok(typeof changeColor !== "undefined", "changeColor() is properly defined.");
+    
+    var spy = sinon.spy(changeColor);
+    
+    $("#changer").trigger("click");
+    changeColor();
+    ok(spy.callCount > 0, "Change color should be called when the changer button is clicked");
+    
+    expect(2);
+  });
+
+  //ADD COLOR
+  test("addColor()", function() {
+    ok(typeof addColor !== "undefined", "addColor() is properly defined.");
+    
+    var currLength = colors.length;
+    
+    addColor();
+    ok( colors.length > currLength, "addColor() adds an item to the colors array" );
+    expect(2);
+  });
+
+
+});
