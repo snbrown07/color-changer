36a5160bf401fb25fca4b446adfa7e94c4118426
snbrown07 <sbrow5081@withs.org>
2016-04-14 13:55:12 +0000

    color-changer

--- solution.js
+++ solution.js
@@ -1,1 +1,14 @@
-
+var colors = ["purple" , "salmon" , "yellow" , "blue"];
+function changeColor(){
+ var randomIndex= Math.floor((Math.random() * colors.length) + 1) - 1;
+ $("body").css("background", colors[randomIndex]);
+}
+function addColor(){
+  var newColors= $("#newColor").val();
+  colors.push(newColors);
+  $("body").css("background", colors[colors.length -1]);
+}
+$(document).ready(function(){
+    $("#change").click(changeColor);
+    $("#add").click(addColor);
+});
